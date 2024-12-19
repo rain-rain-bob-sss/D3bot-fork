ed134aae53aeb40c825df3d71c49a4f2d4ca8f97
bol <110291415+b0ls@users.noreply.github.com>
2024-12-20 00:36:50 +0200

    Update basics.lua
    Fix changing wrong line 💀

--- lua/d3bot/sv_zs_bot_handler/basics.lua
+++ lua/d3bot/sv_zs_bot_handler/basics.lua
@@ -79,10 +79,10 @@ function D3bot.Basics.Walk(bot, pos, aimAngle, slowdown, proximity)
 		if weapon and weapon.GetClimbing and weapon:GetClimbing() and weapon.GetClimbSurface then
 			local tr = weapon:GetClimbSurface()
 			if tr and tr.Hit then
-				bot:D3bot_AngsRotateTo((-tr.HitNormal):Angle(), 0.5)
+				bot:D3bot_AngsRotateTo((-tr.HitNormal):Angle(), 1)
 			end
 		else
-			bot:D3bot_AngsRotateTo(Vector(pos.x-origin.x, pos.y-origin.y, 0):Angle(), 1)
+			bot:D3bot_AngsRotateTo(Vector(pos.x-origin.x, pos.y-origin.y, 0):Angle(), 0.5)
 		end
 	else
 		if mem.BarricadeAttackEntity and mem.BarricadeAttackPos and mem.BarricadeAttackEntity:IsValid() and mem.BarricadeAttackPos:DistToSqr(origin) < 100*100 then
