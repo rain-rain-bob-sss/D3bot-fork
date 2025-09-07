92852d67dd04db92c799db493460de434bda20e7
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2025-09-07 11:10:39 +0800

    fix again

--- lua/d3bot/sv_zs_bot_handler/basics.lua
+++ lua/d3bot/sv_zs_bot_handler/basics.lua
@@ -498,7 +498,7 @@ function D3bot.Basics.Walk(bot, pos, aimAngle, slowdown, proximity)
 			canusestrat1 = false
 			mem.LastJumpTime = CurTime()
 		end
-	elseif attackType == "Cade" and D3bot.Basics.AttackStrat(bot,true) == 1 and canusestrat1 then
+	elseif attackType == "Cade" and D3bot.Basics.AttackStrat(bot,true) == 1 and canusestrat1 and not jumpParam and not jumpToParam then
 		actions.Jump = false
 	end
 
